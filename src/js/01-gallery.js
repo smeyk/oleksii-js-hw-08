9427526bb3319bef9480691d3776f2ac41d25092
Olexii Guberniuk <oleksii@MacBook-Pro-Olexii.local>
2023-10-26 10:14:50 +0200

    add tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+
+const galeryItem = galleryItems
+	.map((image) =>
+		`<li class="gallery__item">
+		<a class="gallery__link" href="${image.original}">
+		<img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+		</a>
+		</li>`
+	)
+	.join("")
+gallery.insertAdjacentHTML("afterbegin", galeryItem);
+
+new simpleLightbox('.gallery__item .gallery__link', {
+	captionsData: "alt",
+	captionDelay: 250,
+});
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form')
+let dataForm = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+const { email, message } = form.elements;
+loadPage();
+
+const checkFields = () => {
+	dataForm = { email: email.value, message: message.value };
+	localStorage.setItem("feedback-form-state", JSON.stringify(dataForm));
+}
+
+function loadPage() {
+	if (dataForm) {
+		email.value = dataForm.email || '';
+		message.value = dataForm.message || '';
+	}
+}
+
+const onFormSubmit = (event) => {
+	event.preventDefault();
+	if (email.value !== "" || message.value !== "") {
+		console.log({ email: email.value, message: message.value })
+	}
+	event.currentTarget.reset();
+	localStorage.removeItem("feedback-form-state");
+	dataForm = {};
+}
+
+form.addEventListener("input", throttle(checkFields, 500));
+form.addEventListener('submit', onFormSubmit);
+
+
 
